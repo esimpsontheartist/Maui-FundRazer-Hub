de4c490ab9868cbe805d8b3528fb17733635dcfa
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-08-11 10:55:31 -1000

    Update and rename 11_👋_7MT_Market_Zero.py to 3_👋_Maui_Art_Market_One.py

---
+++ pages/3_👋_Maui_Art_Market_One.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Maui-Fundraze0 Marketplace One",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Maui-Fundraze0 Marketplace One! 👋")
+
+st.markdown(
+    """
+    ### 
+    - Check out [7MT Market Zero!](https:// /)
+
+
+"""
+)
