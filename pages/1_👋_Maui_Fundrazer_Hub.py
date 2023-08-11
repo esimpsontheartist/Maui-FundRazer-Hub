3ed792e370ec2e17d73f16c4ec33e353338767f3
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-08-11 10:53:04 -1000

    Update and rename 10_👋_7MT_Cross_Chain_Swap.py to 1_👋_Maui_Fundrazer_Hub.py

---
+++ pages/1_👋_Maui_Fundrazer_Hub.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="7MT CrossChain Swap",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Maui Fundarazer Project Information Hub. 👋")
+
+st.markdown(
+    """
+    ### Want to learn more?
+    - Check out [Maui FundRazer Token for Art purchase and donation to Kingdom! Demo](https:///)
+
+
+"""
+)
