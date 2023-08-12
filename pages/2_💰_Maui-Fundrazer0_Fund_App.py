701271e2740244614f587f2946fd91a954a3c81a
esimpsontheartist <92563156+esimpsontheartist@users.noreply.github.com>
2023-08-11 23:40:25 -1000

    Update 2_💰_Maui-Fundrazer0_Fund_App.py

--- pages/2_💰_Maui-Fundrazer0_Fund_App.py
+++ pages/2_💰_Maui-Fundrazer0_Fund_App.py
@@ -10,7 +10,8 @@ st.write("# Welcome to Maui-Fundrazer Fund App Information Page! 💰")
 st.markdown(
     """
     ### Want to learn more?
-    - Check out [For only 10 Matic you can get 1 Maui-FundRaze0 token!  The art in the collections starts at 10 tokens in some cases, 100 in others!  Bid to donate the tokens back to the Kingdom and get art!  Mint the tokens at the Fund Page!]
-(https://maui-lava-raze.vercel.app/)
+    - Check out [For only 10 Matic you can get 1 Maui-FundRaze0 token!  The art in the collections starts at 10 tokens in some cases, 100 in others!  Bid to donate the tokens back to the Kingdom and get art!  Mint the tokens at the Fund Page!](https://maui-lava-raze.vercel.app/) 
+
+
 """
 )
