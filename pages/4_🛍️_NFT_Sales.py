977eb4c30e6f3bddf2d0b96225e417ced0426276
esimpsontheartist <92563156+esimpsontheartist@users.noreply.github.com>
2023-08-13 11:01:03 -1000

    Update 4_🛍️_NFT_Sales.py

--- pages/4_🛍️_NFT_Sales.py
+++ pages/4_🛍️_NFT_Sales.py
@@ -7,22 +7,6 @@ import plotly.subplots as sp
 import data
 
 
-def add_bg_from_url():
-    st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
-             background-attachment: fixed;
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
-
-add_bg_from_url() 
-
 # Global Variables
 theme_plotly = None # None or streamlit
 week_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
@@ -486,3 +470,19 @@ else:
             fig.update_xaxes(categoryorder='category ascending')
             fig.update_yaxes(categoryorder='array', categoryarray=week_days, dtick=2)
             st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url() 
