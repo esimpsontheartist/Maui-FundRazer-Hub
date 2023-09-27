c25bc9c7a71729106c58e66fa22fac7cf7703731
esimpsontheartist <92563156+esimpsontheartist@users.noreply.github.com>
2023-09-26 17:01:52 -1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,13 +3,13 @@ import streamlit as st
 from PIL import Image
 
 # Confit
-st.set_page_config(page_title='Maui Fundrazer Art Charity Project Hub', page_icon=':bar_chart:', layout='wide')
+st.set_page_config(page_title='Maui Fundrazer Info Hub', page_icon=':bar_chart:', layout='wide')
 
 # Title
-st.title('Maui Fundrazer Art Charity Project Hub')
+st.title('Maui Fundraze Info Hub')
 
 # Content
-c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14 = st.columns(14)
+c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15 = st.columns(15)
 c1.image(Image.open('images/ethereum-logo.png'))
 c2.image(Image.open('images/bsc-logo.png'))
 c3.image(Image.open('images/polygon-logo.png'))
@@ -25,18 +25,31 @@ c12.image(Image.open('images/optimism-logo.png'))
 c13.image(Image.open('images/arbitrum-logo.png'))
 c14.image(Image.open('images/axelar-logo.png'))
 
+c16, c17= st.columns(2)
+c16.image(Image.open('images/Screenshot - 8_21_2023 , 3_52_15 PM.png'))
+c17.image(Image.open('images/allenVans.png'))
+
+
 st.write(
     """
-    Maui Fundrazer Tools for Users is here for your viewing needs and pleasure.
-
-
+This Token project for Art and Video Game asset token NFTs is for Royal and Local fundraising alpha testers at this time only.  If you did not register to be part of this process you should not be here!
     """
 )
 
-
-st.subheader('Future Works')
+st.subheader('Methodology')
 st.write(
     """
-Roadmap data coming soon...Games in the future will incorporate your NFTs and art tokens!  Perhapse even game airdrops!
+    The data for this cross-chain comparison were selected via
+    data platform by using its **REST API**. These queries are currently set to **re-run every 24 hours** to cover the latest
+    data and are imported as a JSON file directly to each page. The data were selected with a **1 day delay** for all
+    blockchains to be in sync with one another.
+
+    It is worth mentioning that a considerable portion of the data used for this tool was manually decoded from the raw
+    transaction data on some of the blockchains. Besides that, the names of addresses, DEXs, collections, etc. are also
+    manually labeled. As the queries are updated on a daily basis to cover the most recent data, there is a chance
+    that viewers encounter inconsistent data through the app. Due to the heavy computational power required to execute
+    the queries, and also the size of the raw data being too large, it was not feasible to cover data for a longer period,
+    or by downloading the data and loading it from the repository itself. Therefore, the REST API was selected as the
+    proper form of loading data for the time being.
     """
 )
